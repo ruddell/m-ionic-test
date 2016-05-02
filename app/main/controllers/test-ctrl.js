946895fa3a5581d9d891a2366e6f6358ed22e168
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-05-02 13:37:24 -0600

    Revert "Create a file with gulp watch running"
    This reverts commit a19e6023d31fc370e25cc05e55998bd7db7bc717.

--- app/main/controllers/test-ctrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('main')
-.controller('TestCtrl', function ($log) {
-
-  $log.log('Hello from your Controller: TestCtrl in module main:. This is your controller:', this);
-
-});
